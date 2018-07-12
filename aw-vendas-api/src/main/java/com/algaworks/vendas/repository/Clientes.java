52567ada9f9f8567206d3ba9588a44f17a1e2a5c
Rafael <rafaelbuenogodoy@gmail.com>
2018-07-12 20:54:30 -0300

    Arquivo inicial

---
+++ aw-vendas-api/src/main/java/com/algaworks/vendas/repository/Clientes.java
@@ -0,0 +1,10 @@
+package com.algaworks.vendas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.algaworks.vendas.model.Cliente;
+
+public interface Clientes extends JpaRepository<Cliente, Long> {
+
+	
+}
